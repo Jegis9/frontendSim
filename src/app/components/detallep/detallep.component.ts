206e2caf7f762166788e6a112b65a3332de194b1
Jegis9 <ixcaquic22@gmailcom>
2024-04-01 20:35:00 -0600

    rutas agregadas

---
+++ src/app/components/detallep/detallep.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detallep',
+  standalone: true,
+  imports: [],
+  templateUrl: './detallep.component.html',
+  styleUrl: './detallep.component.css'
+})
+export class DetallepComponent {
+
+}
---
+++ src/app/components/registropago/registropago.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registropago',
+  standalone: true,
+  imports: [],
+  templateUrl: './registropago.component.html',
+  styleUrl: './registropago.component.css'
+})
+export class RegistropagoComponent {
+
+}

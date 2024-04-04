17477f1d56da94776ca9dbccad5bce8e677e720d
DJDaniel8 <98070923+DJDaniel8@users.noreply.github.com>
2024-04-03 23:33:59 -0600

    Add fiture that hide the nav bar when an opcion is clicked

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +8,28 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      document.addEventListener("DOMContentLoaded", () => {
+        this.setupNavBar();
+      });
+    }
+    
+  }
+
+  setupNavBar() {
+    const navLinks = document.querySelectorAll('.navbar .nav-link');
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        const navbarToggle = document.querySelector('.navbar-toggler');
+        if (navbarToggle && window.getComputedStyle(navbarToggle).display !== 'none') {
+          (navbarToggle as HTMLElement).click();
+        }
+      });
+    });
+  }
 }

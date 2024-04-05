91eb53e268590314df3f2b0d85420b4972eb28a1
Jegis9 <ixcaquic22@gmailcom>
2024-04-04 22:32:46 -0600

    Tablas y espacio de graficas agregado correctamente

--- src/app/components/componentsDashboard/slidebar/slidebar.component.ts
+++ src/app/components/componentsDashboard/slidebar/slidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GraficasdashComponent } from '../graficasdash/graficasdash.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { PagosReaDashComponent } from '../pagos-rea-dash/pagos-rea-dash.component';
 
 @Component({
   selector: 'app-slidebar',
   standalone: true,
-  imports: [GraficasdashComponent,RouterOutlet],
+  imports: [GraficasdashComponent,RouterOutlet,GraficasdashComponent,PagosReaDashComponent,RouterLink],
   templateUrl: './slidebar.component.html',
   styleUrl: './slidebar.component.css'
 })

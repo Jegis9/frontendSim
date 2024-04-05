89f37d90e1e216aa966d911ef395d8a68d226682
EricksonSaurio <etacamb@miumg.edu.gt>
2024-04-05 10:51:13 -0600

    se agregan los componentes de card-administrativo

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { AccordionComponent } from '../accordion/accordion.component';
 import { CardComponent } from '../card/card.component';
+import { CardTemaComponent } from '../card-tema/card-tema.component';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [CarouselComponent, AccordionComponent,CardComponent],
+  imports: [CarouselComponent, AccordionComponent,CardComponent,CardTemaComponent],
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })

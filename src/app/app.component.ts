9d23e3c1099525c88fbb3cd78b02fa128285de3b
Jegis9 <ixcaquic22@gmailcom>
2024-04-17 12:12:56 -0600

    Slidebar ya arreglado para dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   isDashboardPage(): boolean {
     return this.router.url.includes('/dashboards');
   }
+  
 }
 
 
--- src/app/components/componentsDashboard/slidebar/slidebar.component.ts
+++ src/app/components/componentsDashboard/slidebar/slidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { GraficasdashComponent } from '../graficasdash/graficasdash.component';
+
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { PagosReaDashComponent } from '../pagos-rea-dash/pagos-rea-dash.component';
+
 
 @Component({
   selector: 'app-slidebar',
   standalone: true,
-  imports: [GraficasdashComponent,RouterOutlet,GraficasdashComponent,PagosReaDashComponent,RouterLink],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './slidebar.component.html',
   styleUrl: './slidebar.component.css'
 })
